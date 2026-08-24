0a181b8df06da260c2e015a3ed0a15f6ae6f8b64
Pedro-Pereira0 <pedrodanielmilheirosilvapereira@outlook.pt>
2026-08-24 21:31:41 +0100

    gitignore so db files dont appear. Creating an engine for sqlalchemy and its initializing the database and creating tables.

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from api.routers.meetingRouter import meeting_router
+from sqlalchemy import create_engine
+from models.base import Base
+from models.meeting import Meeting
+from models.user import User
 
 app = FastAPI()
 app.include_router(meeting_router)
+engine = create_engine("sqlite:///./test.db", echo=True)
 
 if __name__ == "__main__":
     import uvicorn
+    Base.metadata.create_all(engine)
     uvicorn.run(app, host="localhost", port=8000)
