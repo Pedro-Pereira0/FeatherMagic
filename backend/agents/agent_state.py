d78d9aa7b640452d027d407fccc30e9a6d469a4b
Pedro-Pereira0 <pedrodanielmilheirosilvapereira@outlook.pt>
2026-08-31 18:42:31 +0100

    small comment change

--- backend/agents/agent_state.py
+++ backend/agents/agent_state.py
@@ -7,7 +7,7 @@ class AgentState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
     transcription: list[dict] #List[dict]
     context: list[dict] #[{id, time_start, time_end, Theme}]
-    relevant_dialogues: list[dict] #[theme - list[speaker, dialogue]] [str, list[dict]]
+    relevant_dialogues: list[dict] #{theme : dict, dialogues : list[dict]}
     draft: list[dict] #[type of text - text] [str, str]
     iteration: int #num_iteration - prevent infinite loops
 
